256153d6eb3484e6eff3989c10dd20de6bdc9dfe
Martin Marsell <martbustmar@gmail.com>
2023-01-27 19:47:42 +0100

    set up the handlesubmit function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -42,7 +42,33 @@ const CreatePost = () => {
   };
 
   // Submit
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image first');
+    }
+  };
 
   // Change
   const handleChange = (e) => {
