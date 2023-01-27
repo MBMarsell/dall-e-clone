7b6b4a1ae0a9377e194e7a13f0c868760e4c03dd
Martin Marsell <martbustmar@gmail.com>
2023-01-27 15:56:08 +0100

    setup createpost page with state and handler functions

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generateImg, setGenerateImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // Submit
+  const handleSubmit = () => {};
+
+  // Change
+  const handleChange = () => {};
+
+  // Surprise prompt handler
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Create imaginative and visually stunning images through the help of
+          DALL-E and share them with the world.
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Super Mario experiencing hallucinations after having too many shrooms"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
