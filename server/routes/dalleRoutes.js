eb30c3a42b7a0eda002c0e0bd0749a256297efea
Martin Marsell <martbustmar@gmail.com>
2023-01-27 18:57:55 +0100

    set up the dalle route and write the generate image function in the createPost page

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -5,3 +5,35 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: 'Hello AI MOM' });
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
+
+export default router;
